465ef65818232d128a5015e83b2679a38737493a
Brandon Lefore <blefore@energous.com>
2019-10-09 23:55:00 -0700

    Implemented adding objects w/ heapify up

--- maxheap.py
+++ maxheap.py
@@ -1,3 +1,5 @@
+import unittest
+
 from DataStructures.nodes import TreeNode
 
 
@@ -5,14 +7,51 @@ class MaxNodeHeap:
     def __init__(self):
         self.root = TreeNode()
 
-    def __getitem__(self, item):
-        self.__getitem(self.root, item)
+    def add(self, value):
+        if self.root.value is None:
+            self.root = TreeNode(value)
+        else:
+            self._add(self.root, value)
 
-    def __getitem(self, node, item):
-        if node == item:
-            return node
-        if node.left:
-            pass
+    def _add(self, node, value):
+        if node.left is None:
+            node.left = TreeNode(value)
+            node.left.parent = node
+            self.heapify_up(node.left)
+        elif node.right is None:
+            node.right = TreeNode(value)
+            node.right.parent = node
+            self.heapify_up(node.right)
+        else:
+            self._add(node.left, value)
 
-    def __setitem__(self, key, value):
-        pass
+    def swap(self, node1, node2):
+        node1.value, node2.value = node2.value, node1.value
+
+    def heapify_up(self, node):
+        if node.parent is not None:
+            if node.parent < node:
+                self.swap(node.parent, node)
+                self.heapify_up(node.parent)
+
+    def __iter__(self):
+        return iter(self._inorder_traversal(self.root))
+
+    def _inorder_traversal(self, node):
+        if node is not None:
+            if node.left:
+                yield from self._inorder_traversal(node.left)
+            yield node
+            if node.right:
+                yield from self._inorder_traversal(node.right)
+
+
+class TestMaxNodeHeap(unittest.TestCase):
+
+    def test_add(self):
+        tst = MaxNodeHeap()
+        tst.add(10)
+        tst.add(12)
+        print(list(iter(tst)))
+
+
