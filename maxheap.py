e3a2006d0b20a4a966166af8ff7a80adec975ba7
Brandon Lefore <blefore@energous.com>
2019-10-14 21:52:13 -0700

    Added some test cases, finished poll() with get_last, began heapify down

--- maxheap.py
+++ maxheap.py
@@ -25,6 +25,24 @@ class MaxNodeHeap:
         else:
             self._add(node.left, value)
 
+    def peek(self):
+        return self.root
+
+    def poll(self):
+        max_val = self.root.value
+        last_node = self.get_last(self.root)
+        self.root.value = last_node.value
+        self.heapify_down(self.root)
+        return max_val
+
+    def get_last(self, node):
+        if node.right is not None:
+            self.get_last(node.right)
+        elif node.left is not None:
+            self.get_last(node.left)
+        else:
+            return node
+
     def swap(self, node1, node2):
         node1.value, node2.value = node2.value, node1.value
 
@@ -33,6 +51,17 @@ class MaxNodeHeap:
             if node.parent < node:
                 self.swap(node.parent, node)
                 self.heapify_up(node.parent)
+
+    def heapify_down(self, node):
+        if node.left:
+            larger_val = node.left
+            if node.right:
+                if node.right > node.left:
+                    larger_val = node.right
+
+            if node < larger_val:
+                self.swap(node, larger_val)
+                self.heapify_down(larger_val)
 
     def __iter__(self):
         return iter(self._inorder_traversal(self.root))
@@ -48,10 +77,28 @@ class MaxNodeHeap:
 
 class TestMaxNodeHeap(unittest.TestCase):
 
+    def test_peek(self):
+        test_min_heap = MaxNodeHeap()
+        for i in range(7):
+            test_min_heap.add(i)
+        self.assertEqual(test_min_heap.peek(), 6)
+
+    def test_poll(self):
+        test_min_heap = MaxNodeHeap()
+        for i in range(7):
+            test_min_heap.add(i)
+        test_min_heap.poll()
+        self.assertListEqual(list(test_min_heap),
+                             [1, 3, 6, 4, 2, 5])
+        test_min_heap.poll()
+        self.assertListEqual(list(test_min_heap),
+                             [2, 3, 6, 4, 5])
+
     def test_add(self):
-        tst = MaxNodeHeap()
-        tst.add(10)
-        tst.add(12)
-        print(list(iter(tst)))
+        test_min_heap = MaxNodeHeap()
+        for i in range(7):
+            test_min_heap.add(i)
+        print(list(test_min_heap))
+        self.assertListEqual(list(test_min_heap), list(range(7)))
 
 
